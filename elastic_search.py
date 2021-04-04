b55deb983173fd04ae020fa543cb03899086bf66
unknown <shamoun.ahmad@hotmail.com>
2021-04-04 12:35:05 +0100

    added stop word removal

--- elastic_search.py
+++ elastic_search.py
@@ -1,18 +1,32 @@
 import pandas as pd
 import json
 from elasticsearch import Elasticsearch, helpers
+from nltk.corpus import stopwords
+# Run this if running into errors. Not needed every run
+# import nltk
+# nltk.download('stopwords')
 
 # Change the filepath to your filepath
 df = pd.read_csv("./book_data.csv")
 
-# Drop all unused columns
+# Drop all unused columns, get rid of null rows for book_desc (needed for removal of stop words, consider dropping all
+# nulls?)
 df = df.drop(["book_edition", "book_format", "image_url","book_rating_count","book_review_count","book_pages"],axis=1)
+df = df[df['book_desc'].notnull()]
+
+# Stop word removal prior to indexing
+stop = stopwords.words('english')
+df['book_desc'] = df['book_desc'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+# print(df["book_desc"])
+
+
 # Transform the DataFrame to json string
 result = df.to_json(orient="table")
 parsed = json.loads(result)
 
 # Create the elastic search instance
 es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
+
 # Bulk upload the json file to the test_index
 entries=[]
 for i in range(0,len(parsed['data'])):
@@ -30,5 +44,5 @@ if len(entries) > 0:
     helpers.bulk(es, entries)
 
 
-res = es.get(index='test_index',id=0)   
+res = es.get(index='test_index', id=0)
 print(res['_source'])
