f1ab8b12e397aac763702ef70526a2e21d199293
Skoularikis <kgskoul@gmail.com>
2021-04-15 17:40:21 +0300

    test_universal_model

--- elasticsearch_operations.py
+++ elasticsearch_operations.py
@@ -1,20 +1,21 @@
+import numpy as np
+import tensorflow_hub as hub
 from elasticsearch import Elasticsearch, helpers
-import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
 # from word_embeddings.universal import use_universal_model, get_Model
 from tqdm import tqdm
-import numpy as np
-import tensorflow_hub as hub
+
+import preprocessing
 
 es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
 url = "https://tfhub.dev/google/universal-sentence-encoder/4"
 model = hub.load(url)
-index_name='book_test'
+index_name = 'book_test'
 
 def check_if_index_exist(index):
     return es.indices.exists(index=index_name)
 
-def create_qa_index():
+def create_book_test_index():
     # Define the index mapping
     mapping = {
         "mappings": {
@@ -26,89 +27,7 @@ def create_qa_index():
                     "type": "text"  # formerly "string"
                 },
                 "book_isbn": {
-                    "type": "float"
-                },
-                "book_rating": {
-                    "type": "float"
-                },
-                "book_title": {
                     "type": "text"
-                },
-                "genres": {
-                    "type": "text"
-                },
-                "book_desc2": {
-                    "type": "text"
-                },
-                # "desc_vec_sparse": {
-                #     "type": "sparse_vector",
-                #     "dims": 18333
-                # },
-                "desc_vec_dense": {
-                    "type": "dense_vector",
-                    "dims": 512
-                }
-            }
-        }
-    }
-    try:
-        # Create the index if not exists
-        if not es.indices.exists(index_name):
-            # Ignore 400 means to ignore "Index Already Exist" error.
-            es.indices.create(
-                index=index_name, body=mapping  # ignore=[400, 404]
-            )
-            print("Created Index -> covid-qa")
-        else:
-            print("Index book test exists...")
-    except Exception as ex:
-        print(str(ex))
-
-def insert_qa(body):
-    if not es.indices.exists(index_name):
-        create_qa_index()
-    # Insert a record into the es index
-    es.index(index=index_name, body=body)
-    # print("QA successfully inserted...")
-
-
-def process_qa():
-    df = preprocessing.read_csv()
-    # df.dropna(inplace=True, subset=["Answers", "Question"])
-
-    print("\nIndexing QA's...")
-    for index, row in tqdm(df.iterrows()):
-        # Index each qa pair along with the question id and embedding
-        insert_qa({
-            'book_authors': row['book_authors'],
-            'book_desc': row['book_desc'],
-            'book_isbn': row['book_isbn'],
-            'book_rating': row['book_rating'],
-            'book_title': row['book_title'],
-            'genres': row['genres'],
-            'desc_vec_dense': np.asarray(model([row['book_desc']])[0]).tolist(),
-            'desc_id': index
-        })
-
-
-
-# def seach_elastic_universal(query):
-#     use_universal_model(query)
-
-def upload_data_to_elastic_mapping():
-    # Change the filepath to your filepath
-    df = preprocessing.read_csv()
-    mapping = {
-        "mappings": {
-            "properties": {
-                "book_authors": {
-                    "type": "text"  # formerly "string"
-                },
-                "book_desc": {
-                    "type": "text"  # formerly "string"
-                },
-                "book_isbn": {
-                    "type": "float"
                 },
                 "book_rating": {
                     "type": "float"
@@ -133,39 +52,58 @@ def upload_data_to_elastic_mapping():
             }
         }
     }
+    try:
+        # Create the index if not exists
+        if not es.indices.exists(index_name):
+            # Ignore 400 means to ignore "Index Already Exist" error.
+            es.indices.create(
+                index=index_name, body=mapping  # ignore=[400, 404]
+            )
+            print("Created Index -> ", index_name)
+        else:
+            print("Index book test exists...")
+    except Exception as ex:
+        print(str(ex))
 
-    response = es.indices.create(
-        index="book_test",
-        body=mapping,
-        ignore=400  # ignore 400 already exists code
-    )
 
-    if 'acknowledged' in response:
-        if response['acknowledged'] == True:
-            print("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
+def insert_qa(body):
+    if not es.indices.exists(index_name):
+        create_book_test_index()
+    # Insert a record into the es index
+    es.index(index=index_name, body=body)
 
-    # catch API error response
-    elif 'error' in response:
-        print("ERROR:", response['error']['root_cause'])
-        print("TYPE:", response['error']['type'])
 
-    # print out the response:
-    print('\nresponse:', response)
-
-# upload_data_to_elastic_mapping()
-
-def upload_data_to_elastic():
-    # Change the filepath to your filepath
+def process_books():
     df = preprocessing.read_csv()
-    # Drop all unused columns
-    columns_to_drop = ["book_edition", "book_format", "image_url", "book_rating_count", "book_review_count",
-                       "book_pages"]
-    # df = preprocessing.drop_columns(df, columns_to_drop)
-    # df = preprocessing.drop_no_english_sentences(df)
-    df = preprocessing.remove_stopwords(df)
-    df = preprocessing.tf_idf_vctrz(df)
-    # Transform the DataFrame to json string
+    df['desc_vec_dense'] = df['book_desc'].apply(lambda x: model([x])[0].tolist())
     parsed = preprocessing.load_json(df)
+    upload_data_to_elastic(parsed)
+    # print("\nIndexing QA's...")
+    # for index, row in tqdm(df.iterrows()):
+    #     # Index each qa pair along with the question id and embedding
+    #     insert_qa({
+    #         'book_authors': row['book_authors'],
+    #         'book_desc': row['book_desc'],
+    #         'book_isbn': row['book_isbn'],
+    #         'book_rating': row['book_rating'],
+    #         'book_title': row['book_title'],
+    #         'genres': row['genres'],
+    #         'desc_vec_dense': np.asarray(model([row['book_desc']])[0]).tolist(),
+    #         '_id': index
+    #     })
+
+def upload_data_to_elastic(parsed):
+    # # Change the filepath to your filepath
+    # df = preprocessing.read_csv()
+    # # Drop all unused columns
+    # columns_to_drop = ["book_edition", "book_format", "image_url", "book_rating_count", "book_review_count",
+    #                    "book_pages"]
+    # # df = preprocessing.drop_columns(df, columns_to_drop)
+    # # df = preprocessing.drop_no_english_sentences(df)
+    # df = preprocessing.remove_stopwords(df)
+    # df = preprocessing.tf_idf_vctrz(df)
+    # # Transform the DataFrame to json string
+    # parsed = preprocessing.load_json(df)
     entries = []
     for i in range(0, len(parsed['data'])):
         source = parsed['data'][i]
@@ -205,7 +143,6 @@ def search_in_elasticsearch(search_term):
     return res
 
 def cosine_similarity_search(search_term):
-
     v = TfidfVectorizer()
     search_term = [search_term]
     query_vec = v.fit_transform(search_term)
@@ -218,7 +155,7 @@ def cosine_similarity_search(search_term):
             "query": {
                 "script_score": {
                     "query": {
-                        "match_all":{}
+                        "match_all": {}
                     },
                     "script": {
                         "source": "cosineSimilarity(params.query_vector, 'desc_vec') + 1.0",
@@ -233,4 +170,4 @@ def cosine_similarity_search(search_term):
 
 # cosine_similarity_search("this is a test")
 
-# process_qa()
+process_books()
--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 # Run this if running into errors. Not needed every run
 # import nltk
 # nltk.download('stopwords')
@@ -22,11 +23,12 @@ def load_json(df):
 
 def drop_columns(df, columns_to_drop):
     # Drop all unused columns
+
     df = df.drop(columns_to_drop, axis=1)
     return df
 
 
-def drop_no_english_sentences(df):
+def dro_no_english_sentences(df):
     langs = []
 
     # Drop non english rows
@@ -56,45 +58,11 @@ def remove_stopwords(df):
     df['book_desc2'] = df['book_desc2'].str.replace('[^\w\s]', ' ')
     df['genres'] = df['genres'].str.replace('[^\w\s]', ' ')
     df['book_authors'] = df['book_authors'].str.replace('[^\w\s]', ' ')
-
     return df
-
 
 def tf_idf_vctrz(df):
     vectrzr = TfidfVectorizer()
     vector = vectrzr.fit_transform(df['book_desc'])
     vec_array = vector.toarray()
     df['desc_vec_sparse'] = pd.DataFrame(((x,) for x in vec_array), columns=['lists'])
-
     return df
-
-# df = read_csv()
-# df = tf_idf_vctrz(df)
-# print(df['desc_vec'].shape)
-# Testing
-df = read_csv()
-columns_to_drop = ["book_edition", "book_format", "image_url", "book_rating_count", "book_review_count",
-                   "book_pages"]
-# df = drop_columns(df, columns_to_drop)
-# df = drop_no_english_sentences(df)
-df = df[df['book_desc'].notnull()]
-df = tf_idf_vctrz(df)
-print(df['desc_vec_sparse'][3].shape)
-#
-# df.to_csv('./test.csv')
-
-# # idx = df["book_desc"].apply(len).idxmax()
-# # print(idx)
-# # print([len(desc) for desc in df['book_desc']])
-
-# search_term = { 'Test': ['This is a test', 'This too is a test', 'and finally, this is another test']
-# }
-#
-# testframe = pd.DataFrame(search_term, columns=['Test'])
-#
-# v = TfidfVectorizer()
-# check = v.fit_transform(testframe['Test'])
-# check2 = check.toarray()
-#
-# testframe['Column'] = pd.DataFrame(((x,) for x in check2), columns=['lists'])
-# print(testframe)
